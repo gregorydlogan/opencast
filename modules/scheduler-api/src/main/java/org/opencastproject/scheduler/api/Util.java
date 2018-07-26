46b09cbd466a4275ac14482a705489c45f0b7f5a
Greg Logan <gregorydlogan@gmail.com>
2018-07-26 10:34:51 -0600

    MH-12177: Updating tests and implementation to better handle DST scheduling.

--- modules/scheduler-api/src/main/java/org/opencastproject/scheduler/api/Util.java
+++ modules/scheduler-api/src/main/java/org/opencastproject/scheduler/api/Util.java
@@ -22,9 +22,15 @@
 package org.opencastproject.scheduler.api;
 
 import net.fortuna.ical4j.model.Period;
+import net.fortuna.ical4j.model.TimeZoneRegistry;
+import net.fortuna.ical4j.model.TimeZoneRegistryFactory;
 import net.fortuna.ical4j.model.property.RRule;
 
+import org.joda.time.DateTime;
 import org.joda.time.DateTimeConstants;
+import org.joda.time.DateTimeZone;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Calendar;
 import java.util.Date;
@@ -33,6 +39,9 @@ import java.util.List;
 import java.util.TimeZone;
 
 public final class Util {
+
+  private static final Logger logger = LoggerFactory.getLogger(Util.class);
+
   private Util() {
   }
 
@@ -43,59 +52,75 @@ public final class Util {
    *  NOTE: Do not modify this without making the same modifications to the copy of this method in IndexServiceImplTest
    *  I would have moved this to the scheduler-api bundle, but that would introduce a circular dependency :(
    *
-   * @param start
-   *          the start date time
-   * @param end
-   *          the end date
+   * @param startUtc
+   *          the start date time  of the recurrence in UTC
+   * @param endUtc
+   *          the end date of the recurrence in UTC
    * @param duration
    *          the duration
    * @param rRule
    *          the recurrence rule
-   * @param tz
+   * @param tz of the timeZone where this event will be scheduled, i.e. the timeZone of the capture agent
    * @return a list of scheduling periods
    */
-  public static List<Period> calculatePeriods(Date start, Date end, long duration, RRule rRule, TimeZone tz) {
-    final TimeZone utc = TimeZone.getTimeZone("UTC");
-    TimeZone.setDefault(tz);
-    net.fortuna.ical4j.model.DateTime periodStart = new net.fortuna.ical4j.model.DateTime(start);
-    net.fortuna.ical4j.model.DateTime periodEnd = new net.fortuna.ical4j.model.DateTime();
+  public static List<Period> calculatePeriods(Date startUtc, Date endUtc, long duration, RRule rRule, TimeZone tz) {
+    List<Period> events = new LinkedList<>();
+    TimeZoneRegistry registry = TimeZoneRegistryFactory.getInstance().createRegistry();
 
-    Calendar endCalendar = Calendar.getInstance(utc);
-    endCalendar.setTime(end);
-    Calendar calendar = Calendar.getInstance(utc);
-    calendar.setTime(periodStart);
-    calendar.set(Calendar.DAY_OF_MONTH, endCalendar.get(Calendar.DAY_OF_MONTH));
-    calendar.set(Calendar.MONTH, endCalendar.get(Calendar.MONTH));
-    calendar.set(Calendar.YEAR, endCalendar.get(Calendar.YEAR));
-    periodEnd.setTime(calendar.getTime().getTime() + duration);
+    logger.debug("Inbound start of recurrence {} and end of recurrence {}", startUtc, endUtc);
+    DateTime startInTz = new DateTime(startUtc).toDateTime(DateTimeZone.forTimeZone(tz));
+    DateTime endInTz = new DateTime(endUtc).toDateTime(DateTimeZone.forTimeZone(tz));
+
+    net.fortuna.ical4j.model.DateTime periodStartTz = new net.fortuna.ical4j.model.DateTime(startInTz.toDate());
+    net.fortuna.ical4j.model.DateTime periodEndTz = new net.fortuna.ical4j.model.DateTime(endInTz.toDate());
+
+    Calendar endCalendarTz = Calendar.getInstance(tz);
+    endCalendarTz.setTime(periodEndTz);
+
+    Calendar calendarTz = Calendar.getInstance(tz);
+    calendarTz.setTime(periodStartTz);
+
+    calendarTz.set(Calendar.DAY_OF_MONTH, endCalendarTz.get(Calendar.DAY_OF_MONTH));
+    calendarTz.set(Calendar.MONTH, endCalendarTz.get(Calendar.MONTH));
+    calendarTz.set(Calendar.YEAR, endCalendarTz.get(Calendar.YEAR));
+    periodEndTz.setTime(calendarTz.getTime().getTime() + duration);
     duration = duration % (DateTimeConstants.MILLIS_PER_DAY);
 
-    List<Period> events = new LinkedList<>();
+    logger.debug("1-Looking at recurrences for {} to {}, duration {}", periodStartTz.getTime(), periodEndTz.getTime(), duration);
+    // Have to change the TimeZone to UTC for the rRule.getRecur() to work correctly in a non-global TimeZone
+    periodStartTz.setTimeZone(registry.getTimeZone("UTC"));
+    periodEndTz.setTimeZone(registry.getTimeZone("UTC"));
 
-    TimeZone.setDefault(utc);
-    for (Object date : rRule.getRecur().getDates(periodStart, periodEnd, net.fortuna.ical4j.model.parameter.Value.DATE_TIME)) {
+    for (Object date : rRule.getRecur().getDates(periodStartTz, periodEndTz, net.fortuna.ical4j.model.parameter.Value.DATE_TIME)) {
       Date d = (Date) date;
-      Calendar cDate = Calendar.getInstance(utc);
-
-      // Adjust for DST, if start of event
-      if (tz.inDaylightTime(periodStart)) { // Event starts in DST
-        if (!tz.inDaylightTime(d)) { // Date not in DST?
-          d.setTime(d.getTime() + tz.getDSTSavings()); // Adjust for Fall back one hour
+      net.fortuna.ical4j.model.DateTime datePeriod = new net.fortuna.ical4j.model.DateTime(d);
+      Calendar cDate = Calendar.getInstance(registry.getTimeZone("UTC"));
+      cDate.setTime(datePeriod);
+      Calendar tzDate  = Calendar.getInstance(tz);
+      tzDate.setTime(datePeriod);
+      logger.debug("Looking at recurrence date {}, {}", d);
+      // Adjust for DST regardless of end time DST
+      if (tz.inDaylightTime(periodStartTz)) {
+        if (!tz.inDaylightTime(d)) {
+          logger.info("Adjusting " + d + " forward 1 hour");
+          d.setTime(d.getTime() + tz.getDSTSavings());
         }
-      } else { // Event doesn't start in DST
-        if (tz.inDaylightTime(d)) {
-          d.setTime(d.getTime() - tz.getDSTSavings()); // Adjust for Spring forward one hour
-        }
+      } else if (tz.inDaylightTime(d)) {  // Otherwise only adjust special case 
+        // Special case for first Sunday
+        // Sunday DST day bug: https://github.com/ical4j/ical4j/issues/117
+        logger.info("Adjusting " + d + " back 1 hour");
+        d.setTime(d.getTime() - tz.getDSTSavings());
       }
+      Calendar cal =  new Calendar.Builder().setTimeZone(tz).setInstant(d).build();
+      cal.setTimeZone(tz);
+      // update with the updated d
       cDate.setTime(d);
-
-      TimeZone.setDefault(null);
-      Period p = new Period(new net.fortuna.ical4j.model.DateTime(cDate.getTime()),
+      Period p = new Period(new net.fortuna.ical4j.model.DateTime(cDate.getTimeInMillis()),
               new net.fortuna.ical4j.model.DateTime(cDate.getTimeInMillis() + duration));
+
       events.add(p);
-      TimeZone.setDefault(utc);
+      logger.trace("Adding date {} period '{}'", d, p.toString());
     }
-    TimeZone.setDefault(null);
     return events;
   }
 }
--- modules/scheduler-api/src/test/java/org/opencastproject/scheduler/api/UtilTests.java
+++ modules/scheduler-api/src/test/java/org/opencastproject/scheduler/api/UtilTests.java
@@ -18,13 +18,10 @@
  * the License.
  *
  */
-package org.opencastproject.scheduler.impl;
+package org.opencastproject.scheduler.api;
 
 import static org.junit.Assert.assertEquals;
 
-import org.opencastproject.scheduler.api.Util;
-
-import net.fortuna.ical4j.model.DateTime;
 import net.fortuna.ical4j.model.Period;
 import net.fortuna.ical4j.model.property.RRule;
 
@@ -34,6 +31,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
@@ -62,78 +60,114 @@ public class UtilTests {
     String days;
     List<Period> periods;
 
-    // JST
+    // JST - does not observe DST at all
     start = Calendar.getInstance(jst);
     start.set(2016, 2, 25, 22, 0);
     end = Calendar.getInstance(jst);
     end.set(2016, 2, 29, start.get(Calendar.HOUR_OF_DAY), 5);
     durationMillis = (end.get(Calendar.MINUTE) - start.get(Calendar.MINUTE)) * 60 * 1000;
     days = "MO,TU,FR,SA,SU"; // --> Still the same day when switch to UTC (22-9)
-
     periods = generatePeriods(jst, start, end, days, durationMillis);
     assertEquals(5, periods.size());
 
-    // PST
+    // PST - Switches on 2016-03-13 -> this does not go over a DST switchover
     start = Calendar.getInstance(pst);
     start.set(2016, 2, 25, 22, 0);
     end = Calendar.getInstance(pst);
     end.set(2016, 2, 29, start.get(Calendar.HOUR_OF_DAY), 5);
     durationMillis = (end.get(Calendar.MINUTE) - start.get(Calendar.MINUTE)) * 60 * 1000;
     days = "MO,TU,WE,SA,SU"; // --> A day after when switching to UTC (22+8)
-
     periods = generatePeriods(pst, start, end, days, durationMillis);
     assertEquals(5, periods.size());
 
-    // CET
+    // CET - Switches on 2016-03-27 -> this goes over a DST switchover
     start = Calendar.getInstance(cet);
-    start.set(2016, 2, 25, 0, 5);
+    start.set(2016, Calendar.MARCH, 24, 12, 5);
     end = Calendar.getInstance(cet);
-    end.set(2016, 2, 29, start.get(Calendar.HOUR_OF_DAY), 10);
+    end.set(2016, Calendar.MARCH, 30, start.get(Calendar.HOUR_OF_DAY), 10);
     durationMillis = (end.get(Calendar.MINUTE) - start.get(Calendar.MINUTE)) * 60 * 1000;
     days = "MO,TH,FR,SA,SU"; // --> A day before when switch to UCT (0-2)
-
     periods = generatePeriods(cet, start, end, days, durationMillis);
     assertEquals(5, periods.size());
   }
 
   @Test
-  public void calculateDSTChange() throws ParseException {
+  public void calculateDSTSpringForwardChange() throws ParseException {
     Calendar start;
     Calendar end;
     long durationMillis;
     String days;
-    List<Period> periods;
 
-    // CET
-    TimeZone.setDefault(cet);
-    start = Calendar.getInstance(cet);
-    start.set(2016, 2, 24, 0, 5);
-    end = Calendar.getInstance(cet);
-    end.set(2016, 2, 29, start.get(Calendar.HOUR_OF_DAY), 10);
+    // CET->CEST test (March 25 is CET->CEST)
+    TimeZone cetCest = TimeZone.getTimeZone("Europe/Berlin");
+
+    //On Sunday, March 27, 2:00 am CET->CEST
+    start = Calendar.getInstance(cetCest);
+    start.set(2016, Calendar.MARCH, 18, 0, 5);
+    end = Calendar.getInstance(cetCest);
+    end.set(2016, Calendar.APRIL, 7, start.get(Calendar.HOUR_OF_DAY), 10);
     durationMillis = (end.get(Calendar.MINUTE) - start.get(Calendar.MINUTE)) * 60 * 1000;
-    days = "MO,TH,FR,SA,SU";
+    days = "MO,TU,WE,TH,FR,SA,SU";
+    doDSTChangeOverTest(cetCest, start, end, days, durationMillis);
+  }
 
-    periods = generatePeriods(cet, start, end, days, durationMillis);
+  @Test
+  public void calculateDSTFallBackChange() throws ParseException {
+    Calendar start;
+    Calendar end;
+    long durationMillis;
+    String days;
+    TimeZone cetCest = TimeZone.getTimeZone("Europe/Berlin");
+
+    //On Sunday, October 30, 3:00 am, CEST->CET
+    start = Calendar.getInstance(cetCest);
+    start.set(2016, Calendar.OCTOBER, 20, 0, 5);
+    end = Calendar.getInstance(cetCest);
+    end.set(2016, Calendar.NOVEMBER, 8, start.get(Calendar.HOUR_OF_DAY), 10);
+    durationMillis = (end.get(Calendar.MINUTE) - start.get(Calendar.MINUTE)) * 60 * 1000;
+    days = "MO,TU,WE,TH,FR,SA,SU";
+    doDSTChangeOverTest(cetCest, start, end, days, durationMillis);
+  }
+
+  /**
+   * Call with fallback and spring forward scenarios
+   * 
+   * @param tz
+   * @param start
+   * @param end
+   * @param days
+   * @param durationMillis
+   * @throws ParseException 
+   */
+  private void doDSTChangeOverTest(TimeZone tz, Calendar start,
+          Calendar end, String days, long durationMillis) throws ParseException  {
+    SimpleDateFormat simpleDateFormat = new SimpleDateFormat("EE MMM dd HH:mm:ss zzz yyyy");
+    simpleDateFormat.setTimeZone(tz);
+
+    List<Period> periods = generatePeriods(cet, start, end, days, durationMillis);
     for (Period p : periods) {
-      logger.info(p.toString());
+      logger.debug(p.toString());
     }
-    assertEquals(5, periods.size());
-    TimeZone.setDefault(cet);
+    assertEquals("Incorrect number of scheduled events!", 20, periods.size());
     for (Period d : periods) {
-      DateTime dEnd = d.getEnd();
-
-      Date date = new Date(dEnd.getTime());
-      Calendar instance = Calendar.getInstance();
-      instance.setTime(date);
-
-      assertEquals(0, instance.get(Calendar.HOUR_OF_DAY));
+      Calendar instance = Calendar.getInstance(tz);
+      long time = d.getStart().getTime();
+      instance.setTime(new Date(time));
+      logger.debug("Instance {}, calendar hour {}, calendar min {}, zone {}",
+              simpleDateFormat.format(instance.getTime()),
+              instance.get(Calendar.HOUR_OF_DAY),
+              instance.get(Calendar.MINUTE),
+              instance.getTimeZone().getID());
+      assertEquals("Incorrect start hour", 0, instance.get(Calendar.HOUR_OF_DAY));
+      logger.info("Correct start time " + instance.getTime());
     }
-
   }
 
   private List<Period> generatePeriods(TimeZone tz, Calendar start, Calendar end, String days, Long duration)
           throws ParseException {
     Calendar utcDate = Calendar.getInstance(utc);
+    start.setTimeZone(tz);
+    end.setTimeZone(tz);
     utcDate.setTime(start.getTime());
     RRule rRule = new RRule(generateRule(days, utcDate.get(Calendar.HOUR_OF_DAY), utcDate.get(Calendar.MINUTE)));
     return Util.calculatePeriods(start.getTime(), end.getTime(), duration, rRule, tz);
