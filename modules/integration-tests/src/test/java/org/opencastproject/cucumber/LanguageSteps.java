60398d59c1fb487f2f2400c89402ccb4f101f5ad
Greg Logan <gregorydlogan@gmail.com>
2019-04-03 15:16:34 -0600

    Rewrite with better understanding of how the waiters work

--- modules/integration-tests/src/test/java/org/opencastproject/cucumber/LanguageSteps.java
+++ modules/integration-tests/src/test/java/org/opencastproject/cucumber/LanguageSteps.java
@@ -2,19 +2,39 @@ package org.opencastproject.cucumber;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import cucumber.api.java.After;
 import cucumber.api.java.en.Then;
 
 public class LanguageSteps {
 
   private final WebDriver driver = WebDriverFactory.createWebDriver();
+
+  private void isDropdownHidden(By locator) {
+    try {
+      new WebDriverWait(driver, 2)
+              .until(ExpectedConditions.invisibilityOfElementLocated(locator));
+    } catch (TimeoutException e) {
+      fail("The language selection dropdown has not disappeared");
+    }
+  }
+
+  private void isHeaderTranslated(By locator, String expected) {
+    try {
+      new WebDriverWait(driver, 2)
+              .until(ExpectedConditions.textToBePresentInElementLocated(locator, expected));
+    } catch (TimeoutException e) {
+      fail("The header did not translate");
+    }
+  }
 
   @Then("I select the admin language dropdown")
   public void selectLanguageDrop() {
@@ -26,29 +46,20 @@ public class LanguageSteps {
 
   @Then("I select {string} and Events reads {string}")
   public void selectLanguage(String language, String eventsText) {
+    //Select the language
     WebElement element = driver.findElement(By.linkText(language));
     element.click();
-    new WebDriverWait(driver,2L).until(new ExpectedCondition<Boolean>() {
-      public Boolean apply(WebDriver d) {
-        WebElement element = d.findElement(By.cssSelector("#lang-dd > ul"));
-        assertFalse(element.isDisplayed());
-        element = driver.findElement(By.xpath("/html/body/section/div/div[1]/h1"));
-        return element.getText().equalsIgnoreCase(eventsText);
-      }
-    });
+
+    isDropdownHidden(By.cssSelector("#lang-dd > ul"));
+    isHeaderTranslated(By.xpath("/html/body/section/div/div[1]/h1"), eventsText);
   }
 
   @Then("I select {string} and the welcome page reads {string}")
-  public void selectWelcomeLanguage(String language, String eventsText) {
+  public void selectWelcomeLanguage(String language, String welcomeText) {
+    //Select the language
     WebElement element = driver.findElement(By.linkText(language));
     element.click();
-    new WebDriverWait(driver,2L).until(new ExpectedCondition<Boolean>() {
-      public Boolean apply(WebDriver d) {
-        WebElement element = d.findElement(By.cssSelector("#lang-dd > ul"));
-        assertFalse(element.isDisplayed());
-        element = driver.findElement(By.xpath("/html/body/section/div/div/form/div[1]/p/span"));
-        return element.getText().equalsIgnoreCase(eventsText);
-      }
-    });
+    isDropdownHidden(By.cssSelector("#lang-dd > ul"));
+    isHeaderTranslated(By.xpath("/html/body/section/div/div/form/div[1]/p/span"), welcomeText);
   }
 }
--- modules/integration-tests/src/test/java/org/opencastproject/cucumber/LoginSteps.java
+++ modules/integration-tests/src/test/java/org/opencastproject/cucumber/LoginSteps.java
@@ -1,14 +1,15 @@
 package org.opencastproject.cucumber;
 
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import cucumber.api.java.After;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
@@ -37,12 +38,12 @@ public class LoginSteps {
 
   @Then("I am logged in as {string}")
   public void checkUsername(String username) {
-    new WebDriverWait(driver,5L).until(new ExpectedCondition<Boolean>() {
-      public Boolean apply(WebDriver d) {
-        return driver.findElement(By.xpath("//*[@id=\"user-dd\"]/div")).getText()
-                .equals(username);
-      }
-    });
+    try {
+      new WebDriverWait(driver, 2).until(ExpectedConditions
+              .textToBePresentInElementLocated(By.xpath("//*[@id=\"user-dd\"]/div"), username));
+    } catch (TimeoutException e) {
+      fail(username + " did not appear where the logged in username appears");
+    }
   }
 
   @Then("I log out")
@@ -55,15 +56,10 @@ public class LoginSteps {
 
   @Then("I fail login")
   public void failLogin() {
-    new WebDriverWait(driver,5L).until(new ExpectedCondition<Boolean>() {
-      public Boolean apply(WebDriver d) {
-        //Unclear why we need to locate this twice, but the first time you get a stale element reference
-        WebElement element = d.findElement(By.xpath("/html/body/section/div/div/form/div[2]"));
-        element = d.findElement(By.xpath("/html/body/section/div/div/form/div[2]"));
-        element.isDisplayed();
-        return element.getText().equalsIgnoreCase("Incorrect username and / or password");
-      }
-    });
+    WebElement element = (new WebDriverWait(driver, 2))
+            .until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("/html/body/section/div/div/form/div[2]"))));
+    assertTrue("Login failure message missing",
+            element.getText().equalsIgnoreCase("Incorrect username and / or password"));
   }
 
   @Then("I am logged out")
