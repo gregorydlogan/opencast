614b976c526e47ef0089782f26446811b1759d91
Greg Logan <gregorydlogan@gmail.com>
2019-04-17 09:25:19 -0600

    Adding test scripts

---
+++ modules/integration-tests/firefox.sh
@@ -0,0 +1,1 @@
+mvn clean test -Dwebdriver.gecko.driver=/home/greg/tools/selenium/geckodriver -Dwebdriver.chrome.driver=/home/greg/tools/selenium/chromedriver -Dbrowser.formfill.enable=false -Dbrowser=firefox
