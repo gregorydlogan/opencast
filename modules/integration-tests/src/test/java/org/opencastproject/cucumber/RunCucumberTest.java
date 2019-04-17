19906135a17dadddcbfcc6c03999a91a198be8ac
Greg Logan <gregorydlogan@gmail.com>
2019-04-17 09:26:30 -0600

    Focus tag, and an attempt to control order of feature execution.  Order appears to be filename-alphabetical regardless of changes here or in side the feature files.

--- modules/integration-tests/src/test/java/org/opencastproject/cucumber/RunCucumberTest.java
+++ modules/integration-tests/src/test/java/org/opencastproject/cucumber/RunCucumberTest.java
@@ -5,6 +5,12 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty"})
+@CucumberOptions(
+        plugin = {"pretty"},
+        //features = {
+        //        "src/test/resources/org/opencastproject/cucumber/series.feature",
+        //        "src/test/resources/org/opencastproject/cucumber/events.feature"
+        //},
+        tags = "@focus")
 public class RunCucumberTest {
 }
